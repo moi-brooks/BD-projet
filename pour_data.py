d939b6351bbbd13ccf4d93ee14cf017ce96f25fe
othmane <elkaabiothmane4@gmail.com>
2025-05-30 15:18:47 +0100

    Refactor pour_data.py to improve readability and maintainability

--- pour_data.py
+++ pour_data.py
@@ -1,7 +1,11 @@
 import sqlite3
 
 conn = sqlite3.connect("hotel_db.sqlite")
+
 with open("data.sql", "r", encoding="utf-8") as f:
-    conn.executescript(f.read())
+    sql = f.read()
+    conn.executescript(sql)
+
 conn.commit()
 conn.close()
+print("✅ data inserted successfully!")
